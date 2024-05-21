d935170ba6b4bb16e0de8e50fbda9c08d8182664
HR Farmer <58487401+hrfarmer@users.noreply.github.com>
2024-05-20 22:15:40 -0500

    Change session to be grabbed from html template

--- oct/frontend/src/helpers.ts
+++ oct/frontend/src/helpers.ts
@@ -2,11 +2,9 @@ import { Session } from "./types/SessionType";
 
 export async function getSession() {
   try {
-    const res = await fetch("http://localhost:8000/api/session/", {
-      credentials: "same-origin",
-    });
-
-    const data: Session = await res.json();
+    const data: Session = JSON.parse(
+      document.getElementById("data")?.textContent as string
+    );
     return data;
   } catch (err) {
     console.log(err);
--- oct/frontend/urls.py
+++ oct/frontend/urls.py
@@ -1,8 +1,17 @@
 from django.urls import re_path
 from django.shortcuts import render
 
+from tournament.serializers import UserSerializer
+
 def index_view(request):
-    return render(request, 'dist/index.html')
+    if not request.user.is_authenticated:
+        return render(request, 'dist/index.html', {"data": {"isAuthenticated": False, "user": None}})
+    
+    user_serializer = UserSerializer(request.user)
+    serialized_user = user_serializer.serialize()
+    print(f"Serialized user: {serialized_user}")
+
+    return render(request, 'dist/index.html', {"data": {"isAuthenticated": True, "user": serialized_user}})
 
 urlpatterns = [
     re_path(r".*", index_view)
