4a502d47e265c8fa733080883797280e94584c64
HR Farmer <58487401+hrfarmer@users.noreply.github.com>
2024-05-21 18:14:05 -0500

    switch to using createBrowserRouter

--- oct/frontend/src/main.tsx
+++ oct/frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import UserSessionProvider from "./UserSessionProvider.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
@@ -15,21 +15,43 @@ import Bracket from "./routes/bracket/page.tsx";
 
 const queryClient = new QueryClient();
 
+const router = createBrowserRouter([
+  {
+    element: <Header />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/logout",
+        element: <Logout />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/mappools",
+        element: <Mappool />,
+      },
+      {
+        path: "/bracket",
+        element: <Bracket />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <UserSessionProvider>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/mappool" element={<Mappool />} />
-            <Route path="/bracket" element={<Bracket />} />
-          </Routes>
-        </BrowserRouter>
+        <RouterProvider router={router} />
       </UserSessionProvider>
     </QueryClientProvider>
   </React.StrictMode>
