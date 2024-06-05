1d50fd539c587612437213621c6530f7c59a8121
Sheppsu <apc2005osu@gmail.com>
2024-06-04 21:07:54 -0500

    use RelationCachingModel

--- oct/achievements/models.py
+++ oct/achievements/models.py
@@ -1,29 +1,28 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from common import RelationCachingModel
+
 User = get_user_model()
 
-class Team(models.Model):
+class Team(RelationCachingModel):
     name = models.CharField(max_length=32, unique=True)
     icon = models.CharField(max_length=64, null=True)
     invite = models.CharField(max_length=16)
-
-    def get_players_with_user(self):
-        return Player.objects.select_related("user").filter(team=self)
     
 
-class Achievement(models.Model):
+class Achievement(RelationCachingModel):
     name = models.CharField(max_length=32)
     category = models.CharField(max_length=64)
 
 
-class AchievementCompletion(models.Model):
+class AchievementCompletion(RelationCachingModel):
     player = models.ForeignKey("Player", on_delete=models.RESTRICT)
     achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
     time_completed = models.DateTimeField()
     
     
-class Player(models.Model):
+class Player(RelationCachingModel):
     user = models.ForeignKey(User, on_delete=models.RESTRICT)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="players")
     completed_achievements = models.ManyToManyField(Achievement, through=AchievementCompletion, related_name="players_completed")
