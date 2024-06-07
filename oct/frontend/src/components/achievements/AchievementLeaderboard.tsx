501e309e6b83cc64e1323a5d96a2a30d2e2f94b8
Sheppsu <apc2005osu@gmail.com>
2024-06-07 01:09:18 -0500

    load leaderboard from api

--- oct/frontend/src/components/achievements/AchievementLeaderboard.tsx
+++ oct/frontend/src/components/achievements/AchievementLeaderboard.tsx
@@ -1,17 +1,42 @@
+import { useGetTeams } from "src/api/query";
+import { AchievementTeamType } from "src/api/types/AchievementTeamType";
+
+function AchievementEntry(team: AchievementTeamType, placement: number) {
+    const icon = team.icon === null || team.icon === "" ? "https://osu.ppy.sh/images/layout/avatar-guest@2x.png" : team.icon;
+    const placementCls = "placement-container" + ([" first", " second", " third"][placement-1] ?? "");
+    return (
+        <div className="leaderboard-entry">
+            <div className="leaderboard-foreground-container">
+                <div className={placementCls}>
+                    <p className="placement-text">#{placement}</p>
+                </div>
+                <img className="placement-img" src={icon}></img>
+                <p className="placement-text">{team.name}</p>
+            </div>
+            <h1 className="placement-text points">0</h1>
+        </div>
+    );
+}
+
+function* generateEntries(teams: AchievementTeamType[] | null) {
+    if (teams === null) {
+        yield <div>Error occurred</div>;
+        return;
+    }
+
+    for (let i=0; i<teams.length; i++) {
+        yield AchievementEntry(teams[i], i+1);
+    }
+}
+
 export default function AchievementLeaderboard() {
+    // TODO: check for errors
+    const { isPending, data } = useGetTeams();
+
     return (
         <div className="leaderboard">
             <h1>Leaderboard</h1>
-            <div className="leaderboard-entry">
-                <div className="leaderboard-foreground-container">
-                    <div className="placement-container first">
-                        <p className="placement-text">#1</p>
-                    </div>
-                    <img className="placement-img" src="https://a.ppy.sh/14895608?1686006091.jpeg"></img>
-                    <p className="placement-text">This Is A Super Long Team Name</p>
-                </div>
-                <h1 className="placement-text points">8</h1>
-            </div>
+            {isPending ? <div>Loading...</div> : generateEntries(data as AchievementTeamType[] | null)}
         </div>
     );
 }
