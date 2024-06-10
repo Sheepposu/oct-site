60e9961421281aeba82a316418a89635d46ed0eb
HR Farmer <58487401+hrfarmer@users.noreply.github.com>
2024-06-10 13:51:07 -0500

    in progress teams page

--- oct/frontend/src/components/achievements/TeamCard.tsx
+++ oct/frontend/src/components/achievements/TeamCard.tsx
@@ -1,13 +1,14 @@
+import { AchievementTeamType } from "src/api/types/AchievementTeamType";
 import "src/assets/css/achievements/teams.css";
 
-export default function TeamCard() {
+export default function TeamCard({ team }: { team: AchievementTeamType }) {
   return (
     <div className="teams-card-container">
       <p className="teams-placement">#1</p>
       <div className="teams-divider">
         <div style={{ display: "flex", alignItems: "center" }}>
           <div className="teams-icon"></div>
-          <p className="teams-team-name">Super Amazing Team</p>
+          <p className="teams-team-name">{team.name}</p>
         </div>
         <div className="teams-points-container">
           <p className="teams-points">5</p>
--- oct/frontend/src/routes/achievements/teams.tsx
+++ oct/frontend/src/routes/achievements/teams.tsx
@@ -1,16 +1,38 @@
+import AnimatedPage from "src/AnimatedPage";
+import { useGetTeam, useGetTeams } from "src/api/query";
+import { AchievementTeamType } from "src/api/types/AchievementTeamType";
 import "src/assets/css/achievements/teams.css";
 import "src/assets/css/tournaments/tournament/index.css";
 import Button from "src/components/Button";
 
 import TeamCard from "src/components/achievements/TeamCard";
+import { useSession } from "src/util/auth";
 
 export default function TeamsCard() {
+  const session = useSession();
+
+  const teamsResponse = useGetTeams();
+  const teams = teamsResponse.data;
+
+  const ownTeamResponse = useGetTeam();
+  const ownTeam = ownTeamResponse.data as AchievementTeamType;
+  console.log(ownTeamResponse.isLoading);
+
   return (
     <div className="info-container">
       <h1 className="info-title">Teams</h1>
       <p className="info-subtitle">Your Team</p>
       <div className="info-your-team-container">
-        <TeamCard />
+        {session.isAuthenticated == false ? (
+          <div className="teams-card-container">Not Authenticated.</div>
+        ) : ownTeamResponse.isLoading ? (
+          <div>Loading</div>
+        ) : (
+          <AnimatedPage>
+            <TeamCard team={ownTeam} />
+          </AnimatedPage>
+        )}
+
         <div className="info-buttons-container">
           <div>
             <Button color="#06c926">Create Team</Button>
@@ -24,7 +46,13 @@ export default function TeamsCard() {
         </div>
       </div>
       <p className="info-subtitle">Teams</p>
-      <div className="info-teams-container"></div>
+      <div className="info-teams-container">
+        {teams?.map((team, index) => (
+          <AnimatedPage>
+            <TeamCard key={index} team={team} />
+          </AnimatedPage>
+        ))}
+      </div>
     </div>
   );
 }
