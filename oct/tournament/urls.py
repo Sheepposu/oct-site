68c02036e502997c23a1933521f5b7a098201389
Sheppsu <apc2005osu@gmail.com>
2024-06-10 23:23:36 -0500

    temporarily remove dashboard

--- oct/tournament/urls.py
+++ oct/tournament/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path("login", views.login, name="api-login"),
     path("logout", views.logout, name="api-logout"),
-    path("dashboard", views.dashboard, name="api-dashboard"),
+    # path("dashboard", views.dashboard, name="api-dashboard"),
 ]
