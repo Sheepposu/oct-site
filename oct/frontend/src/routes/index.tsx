e2c06adb2a5c476abb9a7820eafbbe622c9b6a4f
HR Farmer <58487401+hrfarmer@users.noreply.github.com>
2024-06-12 00:48:50 -0500

    the worst index page known to man

--- oct/frontend/src/routes/index.tsx
+++ oct/frontend/src/routes/index.tsx
@@ -10,10 +10,29 @@ export default function App() {
   return (
     <AnimatedPage>
       <SessionContext.Provider value={getSessionData()}>
-        <div>
-          <div onClick={logoutRedirect}>
-            logout thing
-          </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <p
+            style={{ fontWeight: 900, fontStyle: "italic", fontSize: "150px" }}
+          >
+            OFFLINE CHAT TOURNAMENT
+          </p>
+          <p
+            style={{
+              fontSize: "30px",
+              fontWeight: "bold",
+              marginBottom: "30px",
+            }}
+          >
+            Click OCAH on the header to do stuff I guess...
+          </p>
+          <div onClick={logoutRedirect}>Click to logout</div>
         </div>
       </SessionContext.Provider>
     </AnimatedPage>
