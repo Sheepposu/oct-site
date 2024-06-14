c54e2f2c9ba968c4654bd9d7d3341701433edeea
Sheppsu <apc2005osu@gmail.com>
2024-06-14 02:03:03 -0400

    add description and beatmap_id

--- oct/frontend/src/api/types/AchievementType.ts
+++ oct/frontend/src/api/types/AchievementType.ts
@@ -2,6 +2,8 @@ export type AchievementType = {
     id: number;
     name: string;
     category: string;
+    description: string;
+    beatmap_id: number | null;
 };
 
 export type AchievementExtendedType = {
